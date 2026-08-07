8270dfb1cf93dcc91196faa96a1abc8d56ef63a4
tabow <tabow@users.noreply.github.com>
2026-08-06 19:32:23 -0600

    Stub out main - basic M1 implementation

--- keybchord/src/main.cpp
+++ keybchord/src/main.cpp
@@ -1,13 +1,10 @@
 #include <Arduino.h>
 
 void setup() {
-    Serial.begin(115200);
-    while (!Serial && millis() < 2000) { delay(10); }
-    Serial.println("KeybChord Pico — M1 Scaffolding");
 }
 
 void loop() {
-    delay(100);
+    delay(1000);
 }
 
 #if defined(ARDUINO_ARCH_RP2040)
@@ -15,6 +12,6 @@ void setup1() {
 }
 
 void loop1() {
-    delay(100);
+    delay(1000);
 }
 #endif
